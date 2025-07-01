9cbc012918d7ed11ba768d1cc806d5993b967a00
0PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2025-07-02 00:24:21 +0200

    fix: update auto-reload script to use GTK 3 for AGS and streamline checksum handling

--- .config/ags/auto-reload.sh
+++ .config/ags/auto-reload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! pgrep -x "ags" > /dev/null; then
-	ags run &
+	ags run --gtk 3 &
 fi
 
 current_checksum=$(find ~/.config/ags -type f -exec md5sum {} \; | sort | md5sum)
@@ -10,8 +10,7 @@ while true; do
 	new_checksum=$(find ~/.config/ags -type f -exec md5sum {} \; | sort | md5sum)
 
 	if [ "$current_checksum" != "$new_checksum" ]; then
-		killall ags gjs && ags run &
-		current_checksum=$new_checksum
+		killall ags gjs && ags run --gtk 3 & current_checksum=$new_checksum
 	fi
 
 	sleep 1
